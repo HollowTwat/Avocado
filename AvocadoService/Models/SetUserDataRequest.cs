45c31bc31fbf7bb960d165c7e89e1d2a27ac7b5c
polyanskiy <n.polyanskiy@rosatom.ru>
2024-12-16 02:56:02 +0300

    add phototype

--- AvocadoService/Models/SetUserDataRequest.cs
+++ AvocadoService/Models/SetUserDataRequest.cs
@@ -13,9 +13,11 @@
         public string location { get; set; }
         public string allergy { get; set; }
         public string lifestyle { get; set; }
+        public string phototype { get; set; }
         public string activity { get; set; }
         public string water_intake { get; set; }
         public string stress { get; set; }
         public string habits { get; set; }
+        
     }
 }
